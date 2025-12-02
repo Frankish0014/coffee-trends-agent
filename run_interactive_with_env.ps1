6cad24f6d27fa8f4822a63a1d0d0dd9d25d3fb74
Frankish0014 <f.ishimwe@alustudent.com>
2025-12-03 00:14:31 +0200

    updated the setup env

---
+++ run_interactive_with_env.ps1
@@ -0,0 +1,4 @@
+# PowerShell script to set environment and run interactive demo
+$env:GOOGLE_API_KEY = "AIzaSyDyk8e7iGvc1nAa_kSXyGPzEieth8LRxLs"
+python run_interactive.py
+
---
+++ run_server_with_env.ps1
@@ -0,0 +1,4 @@
+# PowerShell script to set environment and run server
+$env:GOOGLE_API_KEY = "AIzaSyDyk8e7iGvc1nAa_kSXyGPzEieth8LRxLs"
+python run_server.py
+
